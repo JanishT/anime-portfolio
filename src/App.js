48ef97ca7170309de70e0c80eb27a796d02cacc0
janisht <Janish@globalgov.ae>
2026-05-30 17:16:44 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div
       className="App "
-      style={{
-        textAlign: "center",
-        backgroundColor: "#282c34",
-        color: "white",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    
     >
-      Checking ...
+    {/* <Test/> */}hai
     </div>
   );
 }
